2f25817b47cf8738dde2a5311d317421add58959
jonnathan silva <sjonnathan928@gmail.com>
2022-10-29 19:28:15 -0300

    validating input from login page

--- src/page/Login/index.jsx
+++ src/page/Login/index.jsx
@@ -1,44 +1,85 @@
-const Input = ({ name, type, text, placeholder }) => (
-  <>
-    <label htmlFor={name}>{text}</label>
-    <input
-      type={type}
-      name={name}
-      id={name}
-      placeholder={placeholder}
-      className="bg-grey p-2 outline-none rounded-full"
-    />
-  </>
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+
+const Input = (props) => (
+  <input {...props} className="bg-grey p-2 outline-none rounded-full" />
 );
 
+const validationSchema = yup.object({
+  email: yup.string().required("Digite seu E-mail").email("E-mail inválido"),
+  password: yup.string().required("Digite sua senha"),
+});
+
 export const Login = () => {
+  const formik = useFormik({
+    onSubmit: async (velues) => {
+      await axios({
+        method: "get",
+        auth: { username: velues.email, password: velues.password },
+        baseURL: "http://localhost:5000",
+        url: "/login",
+      }).then(({ data }) => {
+        localStorage.setItem("token", data.accessToken);
+      });
+    },
+    validationSchema,
+    validateOnMount: true,
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <div className="w-full h-full flex flex-col md:flex-row ">
       <section className="flex flex-col flex-1 justify-center space-y-6 p-10 md:p-16">
         <h1 className="text-3xl">Login</h1>
 
-        <form className="space-y-6">
+        <form onSubmit={formik.handleSubmit} className="space-y-6">
           <div className="flex flex-col space-y-2">
+            <label htmlFor="email">E-mail</label>
+
             <Input
               type="text"
               name="email"
               text="E-mail"
               placeholder="Digite seu e-mail"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disabled={formik.isSubmitting}
             />
+
+            {formik.touched.email && formik.errors.email && (
+              <div className="text-red-500 text-sm">{formik.errors.email}</div>
+            )}
           </div>
 
           <div className="flex flex-col space-y-2">
+            <label htmlFor="password">Senha</label>
+
             <Input
               type="password"
               name="password"
               text="Senha"
               placeholder="Digite sua senha"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              disabled={formik.isSubmitting}
             />
+
+            {formik.touched.password && formik.errors.password && (
+              <div className="text-red-500 text-sm">
+                {formik.errors.password}
+              </div>
+            )}
           </div>
 
           <input
             type="submit"
-            value="Entrar"
+            value={formik.isSubmitting ? "Enviando..." : "Entrar"}
             className="w-full bg-blue-500 text-grey cursor-pointer rounded-full p-4"
           />
         </form>
